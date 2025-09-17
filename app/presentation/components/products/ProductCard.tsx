50a82e5b127fab38341efb77475409cbeb740a20
Rubén Martínez Vargas <ruben.martinezv@softtekprojects.com>
2025-09-16 20:53:38 -0600

    Adding the favorite icon functionality, miss to add the icon and it's styles.

--- app/presentation/components/products/ProductCard.tsx
+++ app/presentation/components/products/ProductCard.tsx
@@ -2,9 +2,10 @@
 import { RootStackParams } from '@/app/_layout';
 import { globalStyles } from '@/app/config/app-theme';
 import { Product } from '@/app/core/entities/product.entity';
-import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { NavigationProp, useNavigation, useRoute } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import { useProductUserPersistStore } from '../../hooks/useFavoriteProductsStore';
 
 interface Props {
   product: Product;
@@ -14,12 +15,19 @@ export const ProductCard = ({ product }: Props) => {
 
   //const navigation = useNavigation<NavigationProp<HomeStackParams>>();
   const navigation = useNavigation<NavigationProp<RootStackParams>>();
-
+  const route = useRoute();
+  const {userProductList} = useProductUserPersistStore();
   const [id,setID] = useState(0);
+  const [showFavIcon, setFavIcon] = useState(false);
 
   useEffect(() =>{
     setID(product.id)
   },[]);
+
+  useEffect(() => {
+    const checkFavorites:boolean = userProductList.some(favProduct => favProduct.id === product.id);
+    setFavIcon(checkFavorites);
+  },[userProductList]);
 
   return (
     <Pressable
@@ -34,6 +42,11 @@ export const ProductCard = ({ product }: Props) => {
         <Image style={ styles.image } source={{ uri: product.thumbnail }} />
         <Text style={ globalStyles.title }>{ product.title }</Text>
         <Text style={ globalStyles.price }>${ product.price }</Text>
+        {
+          showFavIcon && route.name!=='Profile' ? 
+            <Text>Icon here :)</Text>
+          : null
+        }
       </View>
     </Pressable>
   )
